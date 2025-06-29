a94bfa480bc4238842cd21f6141c325cfa251dac
Ossping <122410215+Ossping@users.noreply.github.com>
2025-06-28 20:20:12 -0500

    Delete TuPrimeraPaginaZapata directory

--- TuPrimeraPaginaZapata/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.buscar, name='home'),
-    path('categoria/new/', views.crear_categoria, name='crear_categoria'),
-    path('autor/new/', views.crear_autor, name='crear_autor'),
-    path('post/new/', views.crear_post, name='crear_post'),
-    path('buscar/', views.buscar, name='buscar'),
-]
